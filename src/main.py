769c5645bbc9f0a251593f9723bc1415315d6b69
fepelipi <felipe.maturana.g@usach.cl>
2019-07-15 22:35:12 -0400

    QAP GA v1

--- src/main.py
+++ src/main.py
@@ -12,7 +12,6 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from src.config import POPULATION_SIZE, NUMBER_OF_GENERATIONS, DRAW_VISUALIZATION, INPUT_FILE, DRAW_CHART
 from src.crossover import BasicCrossover, Crossover
 from src.data_loading import matrices_size, flow_matrix, distance_matrix
-from src.visualization_drawer import CustomDrawer
 from src.fitness_function import get_normalized_result_of_fitness_function_scores_list, compute_fitness_scores_list
 from src.generate_population import generate_random_population
 from src.mutation import Mutation, BasicMutation
